e7a91db2c39fac1d5dabca9509441e94e2c132bc
LizbethMayumy <mayumy19liss@gmail.com>
2025-02-19 17:14:48 -0500

    Actualizar apps.js y style.css

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos=[];
+
+function mostrarNombresAmigos(elemento,lista){ // Mostrar los nombres en la pantalla
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML="";
+    lista.forEach(amigo => {
+        let nuevoElemento = document.createElement("li");
+        nuevoElemento.innerText = amigo;
+        elementoHTML.appendChild(nuevoElemento);
+    });
+}
+
+function mostrarAmigoSortedo(elemento,texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML=texto;
+}
+
+function agregarAmigo(){ //Agrega amigos a la lista.
+    let nombreAmigo=document.getElementById('amigo').value;
+    if (nombreAmigo!==""){
+        amigos.push(nombreAmigo); //Agrega elementos a la lista
+        // console.log(amigos);
+        mostrarNombresAmigos('#listaAmigos',amigos); 
+     }
+    else{
+        alert("Por favor, ingresa un nombre válido.");
+    }
+    limpiarCaja(); //Limpia los valores ingresados en la caja
+}
+
+
+function limpiarCaja(){ //limpia el dato ingresado para ingresar uno nuevo
+    let valorCaja= document.querySelector('#amigo').value='';
+
+}
+
+function sortearAmigo(){
+    
+    cantidadLista= amigos.length;
+    if (cantidadLista !== 0){
+        let indiceAleatorio = Math.floor(Math.random()*cantidadLista); 
+        // console.log(indiceAleatorio)
+        let nombreSorteado=amigos[indiceAleatorio];
+        // console.log(nombreSorteado);
+        mostrarAmigoSortedo('#resultado',`El amigo secreto sorteado es: ${nombreSorteado}`);
+
+    }
+}
